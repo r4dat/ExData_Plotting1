896c29f59af33f8862afacf66eec27944ba7b110
r4dat <rfourtoss@gmail.com>
2014-06-05 10:50:23 -0500

    Add Data Handling *cex hackery to legennd to scale properly.

--- plot4.R
+++ plot4.R
@@ -1,4 +1,60 @@
+# DATA HANDLING
+# Set string Variables
+localdir=getwd()
+filename="household_power_consumption.txt"
+
+# IF file doesn't exist in working directory, 
+# and no subset dataframe exists, dl and expand data file.
+if(
+  !(
+    file_test(op="-f",x=paste(localdir,'/',filename,sep='')) ||
+      (exists("subpower") && is.data.frame(get("subpower"))))
+){
+  # create a temporary file and a temporary directory on your local disk
+  tf <- tempfile()
+  td <- tempdir()
+  
+  # run the download file function, download as binary..  save the result to the temporary file
+  download.file(
+    "https://d396qusza40orc.cloudfront.net/exdata/data/household_power_consumption.zip",
+    tf ,
+    mode = 'wb' 
+  )
+  
+  # unzip the files to the temporary directory
+  files <- unzip( tf , exdir = getwd() )
+}
+
+require(lubridate)
+require(dplyr)
+
+# Read if dataframe doesn't exist in environment, read in.
+if(!(exists("full_power") && is.data.frame(get("full_power"))))
+{
+  full_power = read.csv(paste(localdir,'/',filename,sep=''),
+                        sep=";",stringsAsFactors=FALSE,na.strings="?")
+}
+
+# If Date column isn't POSIXct, convert via lubridate.
+if(!(is.POSIXct(full_power$Date)))
+{
+  full_power$Date=dmy(full_power$Date)
+}
+
+# Filter
+subpower=filter(full_power,
+                Date>=mdy("02012007"),Date<=mdy("02022007"))
+
+
+
+
+
+# PLOTTING START
+#
+png(filename="plot4.png",width=480,height=480,units="px",type="cairo-png")
+
 # Set 2 x 2 plot matrix.
+# Copy original par settings to restore later.
 opar=par(no.readonly=TRUE)
 par(mfrow=c(2,2))
 
@@ -22,12 +78,16 @@ lines(seq(1,2880),subpower$Sub_metering_3,col="blue")
 # Manually define x-axis.
 axis(side=1,labels=c("Thu","Fri","Sat"),at=c(1,1440,2880))
 # Legend Parameters
+# *cex hackery to get the legend to scale properly. Little bastard.
 legend(
   "topright",
   c("sub_metering_1","sub_metering_2","sub_metering_3"),
   lty=c(1,1,1),
   lwd=c(2,2,2),
-  col=c("black","red","blue")
+  col=c("black","red","blue"),
+  bty="n",
+  pt.cex=.8,
+  cex=.7
 )
 
 # Plot Reactive Power
@@ -36,4 +96,7 @@ plot(x=seq(1:2880),y=subpower$Global_reactive_power,type="l",
 # Manually define x-axis.
 axis(side=1,labels=c("Thu","Fri","Sat"),at=c(1,1440,2880))
 
+par(mfrow=c(1,1))
 par=opar
+
+dev.off()
